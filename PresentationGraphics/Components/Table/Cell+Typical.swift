4b0e89f5aaa6b91d0c2f013ae2d1917a70735059
Vlad Maltsev <v.maltsev@hh.ru>
2024-11-23 22:13:02 +0300

    Preference2

--- PresentationGraphics/Components/Table/Cell+Typical.swift
+++ PresentationGraphics/Components/Table/Cell+Typical.swift
@@ -10,8 +10,9 @@ import Foundation
 extension Table.Cell {
     static func code(id: String = UUID().uuidString, @CodeBuilder code: () -> Code) -> Self {
         Table.Cell(id: id) {
-            CodeView(code: code())
+            CodeView(code: code(), font: Theme.Font.codeExtraSmall)
                 .framed(fill: Theme.Color.backgroundSecondary)
+                .gridColumnAlignment(.leading)
         }
     }
 }
--- PresentationGraphics/Slides/S5_DataFlow/S52_Preference2.swift
+++ PresentationGraphics/Slides/S5_DataFlow/S52_Preference2.swift
@@ -15,13 +15,96 @@ struct S52_Preference2: View, Slide {
     var episodes: [Episode] {
         e00(
             """
+            - Еще добавлю пару слов про reduce
+            - Мы его использовали тут по минимуму для смешения величин
+            - Но то как мы этот метод определим определяет поведение преференса
             """
         )
     }
 
     var body: some View {
         TitleSubtitleLayout(title: "Preferences", subtitle: "reduce") {
-            Panels {
+            Table(header: .init("reduce", "Поведение", "Пример")) {
+                Table.Row {
+                    Table.Cell.code {
+                        """
+                        .preference(Key.self, size)
+                        ...
+                        let value: CGSize = nextValue()
+                        """
+                    }
+
+                    Table.Cell {
+                        Text("Передаем ровно одну величину")
+                            .style(.body)
+                    }
+
+                    Table.Cell {
+                        Text("Чтение размера контейнера")
+                            .style(.body)
+                    }
+                }
+
+                Table.Row {
+                    Table.Cell.code {
+                        """
+                        .preference(Key.self, unreadCount)
+                        ...
+                        let value: Int = value + nextValue()
+                        """
+                    }
+
+                    Table.Cell {
+                        Text("Собираем сумму некой величины")
+                            .style(.body)
+                    }
+
+                    Table.Cell {
+                        Text("Общий счетчик")
+                            .style(.body)
+                    }
+                }
+
+                Table.Row {
+                    Table.Cell.code {
+                        """
+                        .preference(Key.self, [annotation])
+                        ...
+                        value: [Annotation] = value + nextValue()
+                        """
+                    }
+
+                    Table.Cell {
+                        Text("Собираем список всех моделей")
+                            .style(.body)
+                    }
+
+                    Table.Cell {
+                        Text("Отрисовать поверх анотации со всех элементов")
+                            .style(.body)
+                    }
+                }
+
+                Table.Row {
+                    Table.Cell.code {
+                        """
+                        .preference(Key.self, ["timingCurve": .easeIn])
+                        .preference(Key.self, ["duration": 8])
+                        ...
+                        value: [String: Any] = value.merged(with: nextValue())
+                        """
+                    }
+
+                    Table.Cell {
+                        Text("Собираем более сложную модель")
+                            .style(.body)
+                    }
+
+                    Table.Cell {
+                        Text("🤷")
+                            .style(.body)
+                    }
+                }
             }
         }
     }
